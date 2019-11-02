0e521bac72f897fa67fef58105d0c4f2128b2612
unknown <med.ali.salhi0gmail.com>
2019-11-02 10:06:45 +0100

    first commit

---
+++ src/main/java/com/diytracker/app/DiyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.diytracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diytracker/app/DiyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diytracker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
